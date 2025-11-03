5f2be18b39ad768e7d2b73ed013125519c17ae87
Shaifali <shaifali@thoughtwin.com>
2025-11-03 13:27:13 +0530

    Updated signup and login components with new changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,26 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Signup from "./Pages/Signup/signup";
 import Login from "./Pages/Login/login";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [usersData,setUsersData]=useState([]);
+ 
+  const getUsersData = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  }
+};
+
+useEffect(() => {
+  getUsersData();
+}, []);
+
+  
   return (
     <Routes>
       <Route path="/signup" element={<Signup />} />
